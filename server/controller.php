ef4d7e25d29f374805483887e9a6bb834af9b5e6
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-04-22 16:32:29 +0200

    importazione vue 2

--- server/controller.php
+++ server/controller.php
@@ -2,6 +2,11 @@
 /* importo data */
 include __DIR__ . '/data.php';
 
+/* trasformo l'array $albums in un pacchetto json */
 header('Content-type: application/json');
-        echo json_encode($albums);
+        echo json_encode(
+            [
+                'result' => $albums,
+            ]
+        );
 ?>
