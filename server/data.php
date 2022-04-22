fdccb444de1d84a2bcd5f7e3d7e58c173bd0c53f
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-04-22 16:12:00 +0200

    collegamenti vari

---
+++ server/data.php
@@ -0,0 +1,54 @@
+<?php 
+
+$album = [
+    [
+        'title' => 'ciaoneee',
+        'url' => '',
+        'author' => 'ciccio',
+        'year' => '1995'
+    ],
+    [
+        'title' => 'ciaoneee',
+        'url' => '',
+        'author' => 'ciccio',
+        'year' => '1995'
+    ],
+    [
+        'title' => 'ciaoneee',
+        'url' => '',
+        'author' => 'ciccio',
+        'year' => '1995'
+    ],
+    [
+        'title' => 'ciaoneee',
+        'url' => '',
+        'author' => 'ciccio',
+        'year' => '1995'
+    ],
+    [
+        'title' => 'ciaoneee',
+        'url' => '',
+        'author' => 'ciccio',
+        'year' => '1995'
+    ],
+    [
+        'title' => 'ciaoneee',
+        'url' => '',
+        'author' => 'ciccio',
+        'year' => '1995'
+    ],
+    [
+        'title' => 'ciaoneee',
+        'url' => '',
+        'author' => 'ciccio',
+        'year' => '1995'
+    ],
+    [
+        'title' => 'ciaoneee',
+        'url' => '',
+        'author' => 'ciccio',
+        'year' => '1995'
+    ],
+];
+
+?>
